1691828ff4ef09b50eb77c20cd587747a72bd573
leoalmeida95 <leo.almeida.silva@hotmail.com>
2020-04-26 11:54:13 -0300

    removendo os exericios do projeto

--- exercicios-react/src/componentes/Filho.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export default props =>
-<div>
-    <button onClick={() => props.notificarSaida('Shopping')} >Vou sair</button>
-</div>
--- exercicios-react/src/utils/componentes.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export function propsFilhos(props){
-    return React.Children.map(props.children, componenteFilho =>{
-        return React.cloneElement(componenteFilho, {...props})
-    });
-}

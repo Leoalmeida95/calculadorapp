a887fe99f29e0aecad7c945923aecfb3c9233649
leoalmeida95 <leo.almeida.silva@hotmail.com>
2020-04-28 01:04:03 -0300

    definindo contratos

--- todoapp/backend/src/api/todo/todoService.js
+++ todoapp/backend/src/api/todo/todoService.js
@@ -1,6 +1,6 @@
 const Todo = require('./todo')
 
-Todo.methos(['get', 'post', 'put', 'delete'])
+Todo.methods(['get', 'post', 'put', 'delete'])
 Todo.updateOptions({new: true, runValidators: true})
 
 module.exports = Todo
--- todoapp/backend/src/loader.js
+++ todoapp/backend/src/loader.js
@@ -1,2 +1,4 @@
-require('./config/server')
+const server = require('./config/server')
 require('./config/database')
+
+require('./config/routes')(server)//chama a funcao do routes.js passando o service como parametro

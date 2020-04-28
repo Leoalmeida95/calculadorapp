6af1e92e407c3ebe6a32822fa4accddcde1774bb
leoalmeida95 <leo.almeida.silva@hotmail.com>
2020-04-28 19:53:11 -0300

    todo e about

---
+++ todoapp/frontend/src/todo/todo.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class Todo extends Component{
+    render(){
+        return(
+            <>
+            <h1>Todo</h1>
+            </>
+        )
+    }
+}

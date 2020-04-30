2efa7f5f17b932c7a3acd5703201193672979219
leoalmeida95 <leo.almeida.silva@hotmail.com>
2020-04-29 21:41:40 -0300

    navbar

--- todoapp/frontend/src/main/app.jsx
+++ todoapp/frontend/src/main/app.jsx
@@ -2,11 +2,13 @@ import 'modules/bootswatch/dist/pulse/bootstrap.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 import Todo from '../todo/todo'
 import About from '../about/about'
+import Menu from '../template/menu'
 
 import React from 'react'
 
 export default props =>(
     <div className='container'>
+        <Menu></Menu>
         <Todo></Todo>
         <About></About>
     </div>

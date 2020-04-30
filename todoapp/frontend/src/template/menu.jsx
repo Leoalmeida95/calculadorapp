1d874d1355fd770ffb6172fdaba31ca31a0582ed
leoalmeida95 <leo.almeida.silva@hotmail.com>
2020-04-29 21:53:28 -0300

    routes

--- todoapp/frontend/src/template/menu.jsx
+++ todoapp/frontend/src/template/menu.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default props=>
 <div>
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#"><i className="fa fa-calendar-check-o"></i> TodoApp</a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
     </button>

b7b8919d0a12eb65d61552fbc21df419d2d55a0c
leoalmeida95 <leo.almeida.silva@hotmail.com>
2020-04-29 21:59:21 -0300

    pageHeader

--- todoapp/frontend/src/about/about.jsx
+++ todoapp/frontend/src/about/about.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props =>
 <div>
-    <h1>Sobre</h1>
+    <PageHeader name='Sobre' small='Nós'></PageHeader>
+    <h2>Nossa História</h2>
+    <p>Sistema React para cadastro de tarefas</p>
+    <h2>Missão e Visão</h2>
+    <p>Permitir ao Usuário cadastrar todas suas tarefas</p>
 </div>
